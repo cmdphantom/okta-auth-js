43af636643ce6b797aafeb353657f70424785b32
jmelberg <jordan.melberg@okta.com>
2018-06-22 15:49:12 -0700

    Updated per RD review

--- test/util/oauthUtil.js
+++ test/util/oauthUtil.js
@@ -187,7 +187,7 @@ define(function(require) {
       authClient.tokenManager.on('refreshed', function() {
         refreshDeferred.resolve();
       });
-      authClient.tokenManager.on('expired', function() {
+      authClient.tokenManager.on('error', function() {
         refreshDeferred.resolve();
       });
       promise = refreshDeferred.promise;
